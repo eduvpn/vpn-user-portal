5819fce45ca8b1faa61e0069908dfa887bad8302
François Kooman <fkooman@tuxed.net>
2016-10-14 12:32:23 +0200

    update for new api

--- tests/SURFnet/VPN/Portal/Test/TestHttpClient.php
+++ tests/SURFnet/VPN/Portal/Test/TestHttpClient.php
@@ -22,7 +22,7 @@ use RuntimeException;
 
 class TestHttpClient implements HttpClientInterface
 {
-    public function get($requestUri)
+    public function get($requestUri, array $requestOptions = [])
     {
         switch ($requestUri) {
             case 'serverClient/server_pools':
@@ -62,7 +62,7 @@ class TestHttpClient implements HttpClientInterface
         }
     }
 
-    public function post($requestUri, array $postData)
+    public function post($requestUri, array $postData, array $requestOptions = [])
     {
         switch ($requestUri) {
             case 'caClient/add_client_certificate':
