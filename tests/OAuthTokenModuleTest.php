01bc06babd098ff2d27653f22da0b13e4ee6ac53
François Kooman <fkooman@tuxed.net>
2017-01-23 16:55:12 +0100

    update OAuth lib

--- tests/OAuthTokenModuleTest.php
+++ tests/OAuthTokenModuleTest.php
@@ -85,7 +85,7 @@ class OAuthTokenModuleTest extends PHPUnit_Framework_TestCase
     {
         $this->assertSame(
             [
-                'access_token' => 'cmFuZG9tXzE.cmFuZG9tXzI',
+                'access_token' => '12345.cmFuZG9tXzE',
                 'token_type' => 'bearer',
                 'expires_in' => 3600,
             ],
