8d6bc689ac87410157d4ca66eb2f164cc1a71ab3
François Kooman <fkooman@tuxed.net>
2018-12-21 15:29:18 +0100

    update locale tool

--- dev/update_translations.php
+++ dev/update_translations.php
@@ -35,37 +35,45 @@ foreach ($localeFileList as $localeFile) {
         }
     }
 
+    $deletedList = [];
     // check which translations are there, but are no longer needed...
     foreach ($localeData as $k => $v) {
         if (!in_array($k, $sourceStr, true)) {
-            // remove them from the translation file
+            // remove them from the translation file, add them to the
+            // "deleted" array
             unset($localeData[$k]);
+            $deletedList[$k] = $v;
         }
     }
 
     // sort the translations
     ksort($localeData);
+    ksort($deletedList);
 
     // create the locale file
     $output = '<?php'.PHP_EOL.PHP_EOL.'return ['.PHP_EOL;
     foreach ($localeData as $k => $v) {
+        $k = quoteStr($k);
+        $v = quoteStr($v);
         if (empty($v)) {
             $output .= sprintf("    //'%s' => '%s',", $k, $v).PHP_EOL;
         } else {
-            if (false === strpos($v, "'")) {
-                // does not contain single quotes
-                $output .= sprintf("    '%s' => '%s',", $k, $v).PHP_EOL;
-            } elseif (false === strpos($v, '"')) {
-                // does not contain double quotes, great, use those
-                $output .= sprintf("    '%s' => \"%s\",", $k, $v).PHP_EOL;
-            } else {
-                // contains both, bleh!
-                $output .= sprintf("    '%s' => \"%s\",", $k, str_replace('"', '\"', $v)).PHP_EOL;
-            }
+            $output .= sprintf("    '%s' => '%s',", $k, $v).PHP_EOL;
         }
+    }
+    // add the deleted entries as comments
+    foreach ($deletedList as $k => $v) {
+        $k = quoteStr($k);
+        $v = quoteStr($v);
+        $output .= sprintf("    // [DELETED] '%s' => '%s',", $k, $v).PHP_EOL;
     }
     $output .= '];';
 
     // write locale file
     file_put_contents($localeFile, $output);
+}
+
+function quoteStr($str)
+{
+    return str_replace("'", "\'", $str);
 }
