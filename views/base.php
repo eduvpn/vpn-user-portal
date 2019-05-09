29392dbe78c37e4692452f77964e6c142effdace
François Kooman <fkooman@tuxed.net>
2019-05-09 21:06:49 +0200

    rework HTML

--- views/base.php
+++ views/base.php
@@ -9,35 +9,32 @@
     <link href="<?=$this->e($requestRoot); ?>css/screen.css" media="screen" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <h1><?=$this->t('VPN Portal'); ?></h1>
-        <?php if (isset($activeItem)): ?>
+    <header>
+        <nav>
+<?php if (isset($activeItem)): ?>
             <?=$this->insert('menu', ['activeItem' => $activeItem]); ?>
-        <?php endif; ?>
-
+<?php endif; ?>
+        </nav>
+    </header>
+    <main>
         <?=$this->section('content'); ?>
-
-        <hr>
-
-        <div class="footer">
-            <?php if ($this->exists('customFooter')): ?>
-                <?=$this->insert('customFooter'); ?>
-            <?php endif; ?>
-
-            <?php if (1 < count($supportedLanguages)): ?>
-                <form method="post" action="<?=$this->e($requestRoot); ?>setLanguage">
-                    <?php foreach ($supportedLanguages as $k => $v): ?>
-                        <button name="setLanguage" value="<?=$this->e($k); ?>"><?=$this->e($v); ?></button>
-                    <?php endforeach; ?>
-                </form>
-            <?php endif; ?>
-
-            <?php if (!isset($_form_auth_login_page)): ?>
-                <form method="post" action="<?=$this->e($requestRoot); ?>_logout">
-                    <button type="submit"><?=$this->t('Sign Out'); ?></button>
-                </form>
-            <?php endif; ?>
-        </div> <!-- /footer -->
-    </div> <!-- /container -->
+    </main>
+    <footer>
+<?php if ($this->exists('customFooter')): ?>
+        <?=$this->insert('customFooter'); ?>
+<?php endif; ?>
+<?php if (1 < count($supportedLanguages)): ?>
+        <form method="post" action="<?=$this->e($requestRoot); ?>setLanguage">
+<?php foreach ($supportedLanguages as $k => $v): ?>
+            <button name="setLanguage" value="<?=$this->e($k); ?>"><?=$this->e($v); ?></button>
+<?php endforeach; ?>
+        </form>
+<?php endif; ?>
+<?php if (!isset($_form_auth_login_page)): ?>
+        <form method="post" action="<?=$this->e($requestRoot); ?>_logout">
+            <button type="submit"><?=$this->t('Sign Out'); ?></button>
+        </form>
+<?php endif; ?>
+    </footer>
 </body>
 </html>
