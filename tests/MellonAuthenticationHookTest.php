82a8400949b00cab891feff685d049e2e299beba
François Kooman <fkooman@tuxed.net>
2020-01-02 16:53:32 +0100

    add test for MellonAuthenticationHook

---
+++ tests/MellonAuthenticationHookTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2019, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+namespace LC\Portal\Tests;
+
+use LC\Common\Http\Request;
+use LC\Portal\MellonAuthenticationHook;
+use PHPUnit\Framework\TestCase;
+
+class MellonAuthenticationHookTest extends TestCase
+{
+    public function testBasic()
+    {
+        $authHook = new MellonAuthenticationHook(
+            'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_10',
+            null,
+            false,
+            null
+        );
+        $userInfo = $authHook->executeBefore(
+            new Request(
+                [
+                    'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_10' => 'abcdef',
+                ]
+            ),
+            []
+        );
+        $this->assertSame('abcdef', $userInfo->getUserId());
+        $this->assertSame([], $userInfo->getPermissionList());
+    }
+
+    public function testSerialization()
+    {
+        $authHook = new MellonAuthenticationHook(
+            'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_10',
+            null,
+            true,
+            'https://sp.example.org/saml',
+        );
+        $userInfo = $authHook->executeBefore(
+            new Request(
+                [
+                    'MELLON_IDP' => 'https://idp.example.org/saml',
+                    'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_10' => 'abcdef',
+                ]
+            ),
+            []
+        );
+        $this->assertSame('https://idp.example.org/saml!https://sp.example.org/saml!abcdef', $userInfo->getUserId());
+        $this->assertSame([], $userInfo->getPermissionList());
+    }
+
+    public function testPermissionList()
+    {
+        $authHook = new MellonAuthenticationHook(
+            'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_10',
+            'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_7',
+            false,
+            null
+        );
+        $userInfo = $authHook->executeBefore(
+            new Request(
+                [
+                    'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_10' => 'abcdef',
+                    'MELLON_urn:oid:1_3_6_1_4_1_5923_1_1_1_7' => 'a;b',
+                ]
+            ),
+            []
+        );
+        $this->assertSame('abcdef', $userInfo->getUserId());
+        $this->assertSame(['a', 'b'], $userInfo->getPermissionList());
+    }
+}
