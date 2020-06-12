1a1687055926b1e789ce3d0be90cf3bc0d6e55c4
François Kooman <fkooman@tuxed.net>
2020-06-12 10:21:02 +0200

    add translation stats script

---
+++ dev/translation_stats.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2019, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+$localeFileList = glob(dirname(__DIR__).'/locale/*.php');
+
+$emptyList = include dirname(__DIR__).'/locale/empty.php';
+$translationCount = count($emptyList);
+
+foreach ($localeFileList as $localeFile) {
+    $translationList = include $localeFile;
+    if ('empty' === basename($localeFile, '.php')) {
+        continue;
+    }
+    $translationStats[basename($localeFile, '.php')] = sprintf('%3d', (count($translationList) / $translationCount * 100));
+}
+
+asort($translationStats);
+
+foreach ($translationStats as $l => $cnt) {
+    echo $l."\t".sprintf('%3d', $cnt).'%'.PHP_EOL;
+}
