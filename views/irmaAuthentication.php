395707677831c053a0e82baf71a9f40b4745f38f
wvbattum <wvbattum@hotmail.com>
2020-12-25 16:33:31 +0100

    changes

--- views/irmaAuthentication.php
+++ views/irmaAuthentication.php
@@ -1,17 +1,15 @@
 <?php $this->layout('base', ['pageTitle' => $this->t('Sign In')]); ?>
 <?php $this->start('content'); ?>
-<<<<<<< HEAD
 <script src="<?=$this->getAssetUrl($requestRoot, 'js/irma.js'); ?>"></script>
 
 <script type="text/javascript">
-
-const irmaServerUrl = 'http://145.100.181.103:8088';
-
+const irmaServerUrl = '<?=$this->e($irmaServerUrl); ?>';
+const userIdAttribute = '<?=$this->e($userIdAttribute); ?>'
 const irmaRequest = {
   "@context": "https://irma.app/ld/request/disclosure/v2",
   "disclose": [
     [
-      ["pbdf.pbdf.email.email"],
+      [userIdAttribute],
     ]
   ]
 };
@@ -24,12 +22,12 @@ window.onload = function() {
 
 //Get the result and submit the form with the token as value
 function finishUp(result) {
-    document.getElementById("sessionPtr").value = result;
+    document.getElementsByName("irma_auth_token")[0].value = result;
     document.forms["myForm"].submit();
 }
 
 function getSessionPtr() {
-  fetch('http://145.100.181.103:8088/session', {
+  fetch(irmaServerUrl + '/session', {
       method: 'POST', 
       headers : {
         'Content-Type': 'application/json',
@@ -68,25 +66,7 @@ function verificate(pointer) {
 </script>
 <button id="verification" onclick="getSessionPtr()">Verify attribute</button>
 <form id="myForm" method="post" action="<?=$this->e($requestRoot.'/irma/verify');?>">
-<input type="hidden" id="sessionPtr" name="irma_auth_token" value="TOKEN_FROM_JS">
-=======
-<!--
-    irma.js obtained from https://gitlab.science.ru.nl/irma/github-mirrors/irma-frontend-packages/-/jobs/111202/artifacts/browse/irma-frontend/dist
-    @see https://github.com/privacybydesign/irma-frontend-packages/tree/master/irma-frontend
--->
-<script src="<?php echo $this->getAssetUrl($requestRoot, 'js/irma.js'); ?>"></script>
-<script>
-const irmaServerUrl = '<?php echo $this->e($irmaServerUrl); ?>';
-const userIdAttribute = '<?php echo $this->e($userIdAttribute); ?>';
-/*
-    Put IRMA client code here
- */
-</script>
-<!-- verify the IRMA token obtained to complete the authentication -->
-<form method="post" action="<?php echo $requestRoot; ?>_irma/verify">
-    <input type="hidden" name="irma_auth_token" value="abc">
-    <button type="submit">Verify</button>
->>>>>>> upstream/irma
+<input type="hidden" name="irma_auth_token" value="TOKEN_FROM_JS">
 </form>
 
 <?php $this->stop('content'); ?>
