3e7cfbddbfb78bee0e661b347c26ee6f349af11d
François Kooman <fkooman@tuxed.net>
2020-12-30 17:06:12 +0100

    obtain sessionPtr in backend and provide to template

--- views/irmaAuthentication.php
+++ views/irmaAuthentication.php
@@ -6,8 +6,7 @@
 -->
 <script src="<?php echo $this->getAssetUrl($requestRoot, 'js/irma.js'); ?>"></script>
 <script>
-const irmaServerUrl = '<?php echo $this->e($irmaServerUrl); ?>';
-const userIdAttribute = '<?php echo $this->e($userIdAttribute); ?>';
+const sessionPtr = '<?php echo $this->e($sessionPtr); ?>';
 /*
     Put IRMA client code here
  */
