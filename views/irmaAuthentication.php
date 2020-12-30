4d4fa02948d3e7942838624cc2a7c80d7c149102
wvbattum <>
2020-12-30 17:49:10 +0100

    commit for pull

--- views/irmaAuthentication.php
+++ views/irmaAuthentication.php
@@ -3,57 +3,55 @@
 <script src="<?=$this->getAssetUrl($requestRoot, 'js/irma.js'); ?>"></script>
 
 <script type="text/javascript">
-const irmaServerUrl = '<?php echo $this->e($irmaServerUrl); ?>';
-const userIdAttribute = '<?php echo $this->e($userIdAttribute); ?>'
-const token = '<?php echo $this->e($token); ?>'
-const irmaRequest = {
-  "@context": "https://irma.app/ld/request/disclosure/v2",
-  "disclose": [
-    [
-      [userIdAttribute],
-    ]
-  ]
-};
+    const irmaServerUrl = '<?php echo $this->e($irmaServerUrl); ?>';
+    const userIdAttribute = '<?php echo $this->e($userIdAttribute); ?>'
+    const token = 'mysecrettoken'
+    const irmaRequest = {
+      "@context": "https://irma.app/ld/request/disclosure/v2",
+      "disclose": [
+        [
+          [userIdAttribute],
+        ]
+      ]
+    };
 
+    document.addEventListener("DOMContentLoaded", function() {
+      //Get the result and submit the form with the token as value
+        function finishUp(result) {
+            document.getElementById("sessionPointer").value = result;
+            document.forms["myForm"].submit();
+        }
 
+        fetch(irmaServerUrl + '/session', {
+              method: 'POST',
+              headers : {
+                'Content-Type': 'application/json',
+                'Authorization': token
+              },
+              body: JSON.stringify(irmaRequest)
+          })
+          .then(results => results.json())
+          .then(data => {verificate(data.sessionPtr, data.token)})
+          .catch(error => console.log(error));
 
-document.addEventListener("DOMContentLoaded", function() {
-  //Get the result and submit the form with the token as value
-    function finishUp(result) {
-        document.getElementById("sessionPointer").value = result;
-        document.forms["myForm"].submit();
-    }
+        function verificate(pointer, sessionToken) {
+            //IRMA front-end options
+            const irmaFrontend = irma.newPopup({
+                debugging: false,
 
-    fetch(irmaServerUrl + '/session', {
-          method: 'POST',
-          headers : {
-            'Content-Type': 'application/json',
-            'Authorization': token
-          },
-          body: JSON.stringify(irmaRequest)
-      })
-      .then(results => results.json())
-      .then(data => {verificate(data.sessionPtr, data.token)})
-      .catch(error => console.log(error));
-
-    function verificate(pointer, token) {
-        //IRMA front-end options
-        const irmaFrontend = irma.newPopup({
-            debugging: false,
-
-            session: {
-                start: false,
-                mapping: {
-                  sessionPtr: () => pointer
-                },
-                result: false
-            }
-        });
-    }
-    irmaFrontend.start()
-      .then(response => finishUp(token))
-      .catch(error => console.error("Couldn't do what you asked", error));
-});
+                session: {
+                    start: false,
+                    mapping: {
+                      sessionPtr: () => pointer
+                    },
+                    result: false
+                }
+            });
+            irmaFrontend.start()
+              .then(response => finishUp(sessionToken))
+              .catch(error => console.error("Couldn't do what you asked", error));
+        }
+    });
 
 </script>
 <form id="myForm" method="post" action="<?php echo $requestRoot; ?>_irma/verify">
