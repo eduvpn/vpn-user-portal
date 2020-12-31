250da373d799846295beb597306e70c2049d81d5
François Kooman <fkooman@tuxed.net>
2020-12-31 14:12:30 +0100

    put IRMA implementation code in separate file, use dataset to pass sessionPtr

--- views/irmaAuthentication.php
+++ views/irmaAuthentication.php
@@ -5,21 +5,14 @@
     @see https://github.com/privacybydesign/irma-frontend-packages/tree/master/irma-frontend
 -->
 <script src="<?php echo $this->getAssetUrl($requestRoot, 'js/irma.js'); ?>"></script>
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    const sessionPtr = '<?php echo $sessionPtr; ?>';
-    /*
-        Put IRMA client code here
-     */
-});
-</script>
-
+<script src="<?php echo $this->getAssetUrl($requestRoot, 'js/irma_impl.js'); ?>"></script>
 <!--
     the _irma/verify endpoint is triggered after the attribute release with the
     IRMA app is complete. This is only used to inform the backend that the
     IRMA server needs to be queried to obtain the attribute...
 -->
-<form method="post" action="<?php echo $requestRoot; ?>_irma/verify">
-</form>
-
+<div id="irmaAuth" data-session-ptr="<?php echo $this->e($sessionPtr); ?>">
+    <form method="post" action="<?php echo $requestRoot; ?>_irma/verify">
+    </form>
+</div>
 <?php $this->stop('content'); ?>
---
+++ web/js/irma_impl.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sessionPtr = document.getElementById('irmaAuth').dataset.sessionPtr;
+    console.log(JSON.parse(sessionPtr));
+    /*
+        Put IRMA client code here
+     */
+});
