092ab5981e1483800f612ff1b00a48a4e06b7e3a
François Kooman <fkooman@tuxed.net>
2021-01-01 18:36:59 +0100

    add IRMA JS

--- web/js/irma_impl.js
+++ web/js/irma_impl.js
@@ -1,9 +1,22 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-    const sessionPtr = document.getElementById('irmaAuth').dataset.sessionPtr;
-    console.log(JSON.parse(sessionPtr));
-    /*
-        Put IRMA client code here
-     */
+    const sessionPtr = document.getElementById("irmaAuth").dataset.sessionPtr;
+    const irmaFrontend = irma.newPopup({
+        debugging: false,
+        session: {
+            start: false,
+            mapping: {
+                sessionPtr: function() {
+                    JSON.parse(sessionPtr);
+                }
+            },
+            result: false
+        }
+    });
+    irmaFrontend.start().then(function(response) {
+        document.querySelector("div#irmaAuth form").submit();
+    }).catch(function(error) {
+        console.error("Couldn't do what you asked 😢", error);
+    });
 });
