7ce0b6700eb64293f28269bdd9db05d30ded4900
François Kooman <fkooman@tuxed.net>
2021-04-23 23:46:01 +0200

    forgot to add PhpCookie.php

---
+++ src/Http/PhpCookie.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+namespace LC\Portal\Http;
+
+class PhpCookie implements CookieInterface
+{
+    private array $cookieOptions;
+
+    public function __construct(bool $secureCookie, string $cookiePath)
+    {
+        $this->cookieOptions = [
+            'expires' => time() + 90 * 60 * 60 * 24,
+            'path' => $cookiePath,
+            'secure' => $secureCookie,
+            'httponly' => true,
+            'samesite' => 'Lax',
+        ];
+    }
+
+    public function get(string $cookieName): ?string
+    {
+        if (!\array_key_exists($cookieName, $_COOKIE)) {
+            return null;
+        }
+        if (!\is_string($_COOKIE[$cookieName])) {
+            return null;
+        }
+
+        return $_COOKIE[$cookieName];
+    }
+
+    public function set(string $cookieName, string $cookieValue): void
+    {
+        setcookie($cookieName, $cookieValue, $this->cookieOptions);
+    }
+}
