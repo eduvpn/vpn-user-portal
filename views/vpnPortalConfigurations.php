de2b9c908e3df0f4e296907c6ece3fe7cd55d203
François Kooman <fkooman@tuxed.net>
2021-06-29 17:17:01 +0200

    more cleanups

--- views/vpnPortalConfigurations.php
+++ views/vpnPortalConfigurations.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1); ?>
 <?php /** @var \LC\Portal\Tpl $this */?>
 <?php /** @var \DateTimeImmutable $expiryDate */?>
+<?php /** @var array<string,\LC\Portal\ProfileConfig> $profileConfigList */?>
+<?php /** @var array<array{profile_id:string,display_name:string,public_key:string,ip_four:string,ip_six:string,expires_at:\DateTimeImmutable,auth_key:?string}>|array<array{profile_id:string,common_name:string,display_name:string,expires_at:\DateTimeImmutable,auth_key:?string}> $userCertificateList */?>
 <?php $this->layout('base', ['activeItem' => 'configurations', 'pageTitle' => $this->t('Configurations')]); ?>
 <?php $this->start('content'); ?>
     <h2><?=$this->t('New'); ?></h2>
@@ -42,27 +44,37 @@
                 <tr><th><?=$this->t('Profile'); ?></th><th><?=$this->t('Name'); ?></th><th><?=$this->t('Expires'); ?></th><th></th></tr>
             </thead>
             <tbody>
-            <?php foreach ($userCertificateList as $userCertificate): ?>
+<?php foreach ($userCertificateList as $userCertificate): ?>
+<?php if (array_key_exists('common_name', $userCertificate)): ?>
+                <!-- OpenVPN -->
                 <tr>
                     <td><?=$this->e($userCertificate['profile_id']); ?></td>
                     <td><span title="<?=$this->e($userCertificate['display_name']); ?>"><?=$this->etr($userCertificate['display_name'], 25); ?></span></td>
                     <td><?=$this->d($userCertificate['expires_at']); ?></td>
                     <td class="text-right">
-<?php if (array_key_exists('common_name', $userCertificate)): ?>
                         <form class="frm" method="post" action="deleteOpenVpnConfig">
                             <input type="hidden" name="commonName" value="<?=$this->e($userCertificate['common_name']); ?>">
                             <button class="warning" type="submit"><?=$this->t('Delete'); ?></button>
                         </form>
-<?php else: ?>
+                    </td>
+                </tr>
+<?php endif; ?>
+<?php if (array_key_exists('public_key', $userCertificate)): ?>
+                <!-- WireGuard -->
+                <tr>
+                    <td><?=$this->e($userCertificate['profile_id']); ?></td>
+                    <td><span title="<?=$this->e($userCertificate['display_name']); ?>"><?=$this->etr($userCertificate['display_name'], 25); ?></span></td>
+                    <td><?=$this->d($userCertificate['expires_at']); ?></td>
+                    <td class="text-right">
                         <form class="frm" method="post" action="deleteWireGuardConfig">
                             <input type="hidden" name="profileId" value="<?=$this->e($userCertificate['profile_id']); ?>">
                             <input type="hidden" name="publicKey" value="<?=$this->e($userCertificate['public_key']); ?>">
                             <button class="warning" type="submit"><?=$this->t('Delete'); ?></button>
                         </form>
-<?php endif; ?>
                     </td>
                 </tr>
-            <?php endforeach; ?>
+<?php endif; ?>
+<?php endforeach; ?>
             </tbody>
         </table>
     <?php endif; ?>
