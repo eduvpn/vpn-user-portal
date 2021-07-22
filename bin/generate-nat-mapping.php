576b390ba94deb7e2225beea23c357eaa134e05c
François Kooman <fkooman@tuxed.net>
2021-07-22 16:40:20 +0200

    give generate-nat-mapping more options

--- bin/generate-nat-mapping.php
+++ bin/generate-nat-mapping.php
@@ -15,14 +15,14 @@ $baseDir = dirname(__DIR__);
 use LC\Portal\Config;
 use LC\Portal\IP;
 
-$firstPort = 10000;
-$portsPerHost = 256;
-
 try {
     $ipFour = true;
     $ipSix = false;
     $externalIpFour = '192.0.2.1';
     $externalIpSix = '2001:db8::1';
+    $portsPerHost = 256;
+    $firstPort = 10000;
+
     for ($i = 1; $i < $argc; ++$i) {
         if ('-4' === $argv[$i]) {
             $ipFour = true;
@@ -32,7 +32,6 @@ try {
             $ipSix = true;
             continue;
         }
-
         if ('--external-v4' === $argv[$i]) {
             if ($i + 1 < $argc) {
                 $externalIpFour = $argv[$i + 1];
@@ -45,8 +44,30 @@ try {
             }
             continue;
         }
+        if ('--ports-per-host' === $argv[$i]) {
+            if ($i + 1 < $argc) {
+                $portsPerHost = (int) $argv[$i + 1];
+            }
+            continue;
+        }
+        if ('--first-port' === $argv[$i]) {
+            if ($i + 1 < $argc) {
+                $firstPort = (int) $argv[$i + 1];
+            }
+            continue;
+        }
         if ('--help' === $argv[$i] || '-h' === $argv[$i]) {
-            echo 'SYNTAX: '.$argv[0].' [-4] [-6] [--external-v4 EXTERNAL_IP] [--external-v6 EXTERNAL_IP]'.\PHP_EOL;
+            $appName = $argv[0];
+            echo <<< EOF
+                SYNTAX: $appName
+                            [-4]                            only IPv4
+                            [-6]                            only IPv6
+                            [--ports-per-host N]            ports per VPN client (256)
+                            [--first-port N]                first port (10000)
+                            [--external-v4 EXTERNAL_IP]     external IPv4 address
+                            [--external-v6 EXTERNAL_IP]     external IPv6 address
+
+                EOF;
             exit(0);
         }
     }
@@ -55,10 +76,17 @@ try {
     if ($ipFour) {
         $clientIpCount = 0;
         echo '###############################################################################'.\PHP_EOL;
-        echo '# IPv4                                                                        #'.\PHP_EOL;
-        echo '###############################################################################'.\PHP_EOL;
+        echo '# IPv4'.\PHP_EOL;
+        echo '#'.\PHP_EOL;
+        echo '# Ports per Host: '.$portsPerHost.\PHP_EOL;
+        echo '# First Port    : '.$firstPort.\PHP_EOL;
+        echo '# External IP   : '.$externalIpFour.\PHP_EOL;
+        echo '###############################################################################'.\PHP_EOL.\PHP_EOL;
         foreach ($config->profileConfigList() as $profileConfig) {
-            echo '# "'.$profileConfig->displayName().'" ('.$profileConfig->profileId().')'.\PHP_EOL;
+            echo \PHP_EOL;
+            echo '###############################################################################'.\PHP_EOL;
+            echo '# Profile: "'.$profileConfig->displayName().'" ('.$profileConfig->profileId().')'.\PHP_EOL;
+            echo '###############################################################################'.\PHP_EOL;
             $ipFourRange = IP::fromIpPrefix($profileConfig->range());
             $splitCount = count($profileConfig->vpnProtoPorts());
             $ipFourSplitRangeList = $ipFourRange->split($splitCount);
@@ -78,10 +106,17 @@ try {
     if ($ipSix) {
         $clientIpCount = 0;
         echo '###############################################################################'.\PHP_EOL;
-        echo '# IPv6                                                                        #'.\PHP_EOL;
+        echo '# IPv6'.\PHP_EOL;
+        echo '#'.\PHP_EOL;
+        echo '# Ports per Host: '.$portsPerHost.\PHP_EOL;
+        echo '# First Port    : '.$firstPort.\PHP_EOL;
+        echo '# External IP   : '.$externalIpSix.\PHP_EOL;
         echo '###############################################################################'.\PHP_EOL;
         foreach ($config->profileConfigList() as $profileConfig) {
-            echo '# "'.$profileConfig->displayName().'" ('.$profileConfig->profileId().')'.\PHP_EOL;
+            echo \PHP_EOL;
+            echo '###############################################################################'.\PHP_EOL;
+            echo '# Profile: "'.$profileConfig->displayName().'" ('.$profileConfig->profileId().')'.\PHP_EOL;
+            echo '###############################################################################'.\PHP_EOL;
             $ipFourRange = IP::fromIpPrefix($profileConfig->range());
             $ipSixRange = IP::fromIpPrefix($profileConfig->range6());
             $splitCount = count($profileConfig->vpnProtoPorts());
