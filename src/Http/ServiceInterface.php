569b17976488039232f54577681d4cdc7985cbda
François Kooman <fkooman@tuxed.net>
2021-08-17 11:19:42 +0200

    add src/Http/ServiceInterface.php

---
+++ src/Http/ServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+namespace LC\Portal\Http;
+
+interface ServiceInterface
+{
+    public function get(string $pathInfo, callable $callback): void;
+
+    public function post(string $pathInfo, callable $callback): void;
+
+    public function postBeforeAuth(string $pathInfo, callable $callback): void;
+}
