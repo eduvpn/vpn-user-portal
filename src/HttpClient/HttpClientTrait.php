0962506d465d4be813f2785af547cc54ca63ee9d
François Kooman <fkooman@tuxed.net>
2021-09-23 22:38:09 +0200

    update docs

--- src/HttpClient/HttpClientTrait.php
+++ src/HttpClient/HttpClientTrait.php
@@ -15,7 +15,8 @@ trait HttpClientTrait
 {
     /**
      * Properly encode HTTP (POST) query parameters while also supporting
-     * duplicate key names.
+     * duplicate key names. PHP's built in http_build_query uses weird key[]
+     * syntax that I am not a big fan of.
      *
      * @param array<string,array<string>|string> $postParameters
      */
