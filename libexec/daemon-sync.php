79b99e35796196554032fcb18cf9bf38e0505924
François Kooman <fkooman@tuxed.net>
2021-09-30 14:45:15 +0200

    add documentation

--- libexec/daemon-sync.php
+++ libexec/daemon-sync.php
@@ -17,19 +17,23 @@ use LC\Portal\HttpClient\CurlHttpClient;
 use LC\Portal\Storage;
 use LC\Portal\VpnDaemon;
 
-// XXX also disconnect *openvpn* clients, not just WG, but that is mostly in
-// order to kick clients that are currently connected with expired certificates?
-// should this script also delete/disconnect *expired* certificates/public keys from the database? that sounds smart
-// then we can get rid of libexec/disconnect-expired-certificates and not longer need housekeeping to delete certs/public keys
-// housekeeping could then run daily to remove oauth session and log entries
-// this sync script could run every 5 minutes for example taking care of:
-// 1. make sure wg is in sync with db
-// 2. remove old certificates / peer configs from db, and then also
-// 3. disconnect openvpn clients with certs that are no longer valid and remove peers no longer valid
-//
-// or maybe simply have 1 housekeeping cron job that runs every 5 minutes that
-// does everything? that may make more sense and it is easier to have only 1
-// script instead of 2, or 3
+/*
+ * This script is responsible for three things:
+ * 1. (Re)add WireGuard peers when they are missing, e.g. after a node reboot
+ * 2. Delete WireGuard peers with expired configurations
+ * 3. Disconnect OpenVPN clients with expired certificates
+ *
+ * This script interfaces with `vpn-daemon` running on the node(s). It will
+ * first figure out which peers/clients should be there and remove/disconnect
+ * the ones that should NOT be there (anymore).
+ *
+ * Due to the architecture, e.g. multiple profiles can use the same vpn-daemon
+ * and the vpn-daemon has no concept of "profiles" the administration is a bit
+ * complicated. A list of peers/clients is created linked to a "nodeBaseUrl",
+ * i.e. the URL for connecting to vpn-daemon that belongs to a profile and
+ * afterwards perform add/remove/disconnect what is necessary.
+ */
+
 try {
     $config = Config::fromFile($baseDir.'/config/config.php');
     $storage = new Storage(
@@ -41,34 +45,35 @@ try {
         $baseDir.'/schema'
     );
     $vpnDaemon = new VpnDaemon(new CurlHttpClient());
-
     foreach ($config->profileConfigList() as $profileConfig) {
-        if ('wireguard' !== $profileConfig->vpnProto()) {
-            continue;
+        if ('wireguard' === $profileConfig->vpnProto()) {
+            $wPeerListByProfileId = $storage->wPeerListByProfileId($profileConfig->profileId(), Storage::EXCLUDE_EXPIRED);
+            $wPeerList = $vpnDaemon->wPeerList($profileConfig->nodeBaseUrl(), true);
+
+            // register peers in WG that are (still) missing
+            $publicKeyList = array_diff(array_keys($wPeerListByProfileId), array_keys($wPeerList));
+            foreach ($publicKeyList as $publicKey) {
+                $vpnDaemon->wPeerAdd(
+                    $profileConfig->nodeBaseUrl(),
+                    $publicKey,
+                    $wPeerListByProfileId[$publicKey]['ip_four'],
+                    $wPeerListByProfileId[$publicKey]['ip_six']
+                );
+            }
+
+            // remove peers from WG that are no longer there (or expired)
+            // **XXX**
         }
 
-        // 1. get list of peers for this profile
-        // 2. get list of all peers known to WG for this nodebaseurl
-        // 3. figure out which ones are missing, add those to WG
-        // 4. figure out which ones are no longer there, and remove those <-- XXX todo
+        if ('openvpn' === $profileConfig->vpnProto()) {
+            // disconnect OpenVPN clients with expired certificate
+            $oCertListByProfileId = $storage->oCertListByProfileId($profileConfig->profileId(), Storage::EXCLUDE_EXPIRED);
+            $oConnectionList = $vpnDaemon->oConnectionList($profileConfig->nodeBaseUrl());
 
-        $wPeerListByProfileId = $storage->wPeerListByProfileId($profileConfig->profileId(), Storage::EXCLUDE_EXPIRED);
-        $wPeerList = $vpnDaemon->wPeerList($profileConfig->nodeBaseUrl(), true);
-
-        // find the peers that are in the database, but not know by the daemon
-        foreach (array_diff(array_keys($wPeerListByProfileId), array_keys($wPeerList)) as $publicKey) {
-            $vpnDaemon->wPeerAdd(
-                $profileConfig->nodeBaseUrl(),
-                $publicKey,
-                $wPeerListByProfileId[$publicKey]['ip_four'],
-                $wPeerListByProfileId[$publicKey]['ip_six']
-            );
+            // disconnect all CNs not in $oCertListByProfileId
+            // but the daemon will portentially return more than just this profile...
+            // **XXX**
         }
-
-        // XXX we also need to *remove* peers that are known by the daemon, but
-        // that we don't have in the database...
-        // however, as one nodeBaseUrl() can have multiple profiles on it, we
-        // need to keep track which ones really need to be removed (somehow)
     }
 } catch (Exception $e) {
     echo 'ERROR: '.$e->getMessage().\PHP_EOL;
