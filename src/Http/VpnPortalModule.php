5a52aab3d8929e1cb6956ba594b1eb990515a064
François Kooman <fkooman@tuxed.net>
2021-10-12 17:01:54 +0200

    check the array_keys of Tpl::supportedLanguages()

--- src/Http/VpnPortalModule.php
+++ src/Http/VpnPortalModule.php
@@ -207,7 +207,7 @@ class VpnPortalModule implements ServiceModuleInterface
         $service->postBeforeAuth(
             '/setLanguage',
             function (Request $request): Response {
-                $this->cookie->set('L', $request->requirePostParameter('uiLanguage', fn (string $s) => Validator::inSet($s, Tpl::supportedLanguages())));
+                $this->cookie->set('L', $request->requirePostParameter('uiLanguage', fn (string $s) => Validator::inSet($s, array_keys(Tpl::supportedLanguages()))));
 
                 return new RedirectResponse($request->requireHeader('HTTP_REFERER'), 302);
             }
