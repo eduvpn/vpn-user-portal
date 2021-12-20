02652704ff3acc6f1a3bcd84b4b5d2e62d67dd36
François Kooman <fkooman@tuxed.net>
2021-12-20 15:59:10 +0100

    simplify Migration class

--- src/Migration.php
+++ src/Migration.php
@@ -28,7 +28,6 @@ class Migration
 
     public function __construct(PDO $dbh, string $schemaDir, string $schemaVersion)
     {
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->dbh = $dbh;
         $this->schemaDir = $schemaDir;
         $this->schemaVersion = self::validateSchemaVersion($schemaVersion);
@@ -61,7 +60,7 @@ class Migration
             throw new RuntimeException(sprintf('unable to read schema directory "%s"', $this->schemaDir));
         }
 
-        $hasForeignKeys = $this->lock();
+        $this->lock();
 
         try {
             foreach ($migrationList as $migrationFile) {
@@ -90,12 +89,12 @@ class Migration
             }
         } catch (Exception $e) {
             // something went wrong that was not related to SQL queries
-            $this->unlock($hasForeignKeys);
+            $this->unlock();
 
             throw $e;
         }
 
-        $this->unlock($hasForeignKeys);
+        $this->unlock();
 
         $currentVersion = $this->getCurrentVersion();
         if ($currentVersion !== $this->schemaVersion) {
@@ -140,60 +139,31 @@ class Migration
         return $fileName;
     }
 
-    private function disableForeignKeys(): void
-    {
-        $this->dbh->exec('PRAGMA foreign_keys = OFF');
-    }
-
-    private function enableForeignKeys(): void
-    {
-        $this->dbh->exec('PRAGMA foreign_keys = ON');
-    }
-
-    private function hasForeignKeys(): bool
-    {
-        // XXX refactor code and at the very least rename it as MySQL et al do
-        // have foreign keys and can't simply be disabled as far as I know...
-        // they will have *different* migration files as they support proper
-        // ALTER TABLE stuff...
-        if ('sqlite' !== $this->dbh->getAttribute(PDO::ATTR_DRIVER_NAME)) {
-            return false;
-        }
-
-        $sth = $this->dbh->query('PRAGMA foreign_keys');
-        $hasForeignKeys = '1' === $sth->fetchColumn(0);
-        $sth->closeCursor();
-
-        return $hasForeignKeys;
-    }
-
-    private function lock(): bool
+    private function lock(): void
     {
         // this creates a "lock" as only one process will succeed in this...
         $this->dbh->exec('CREATE TABLE _migration_in_progress (dummy INTEGER)');
 
-        if ($hasForeignKeys = $this->hasForeignKeys()) {
-            $this->disableForeignKeys();
+        if ('sqlite' === $this->dbh->getAttribute(PDO::ATTR_DRIVER_NAME)) {
+            $this->dbh->exec('PRAGMA foreign_keys = OFF');
         }
-
-        return $hasForeignKeys;
     }
 
-    private function unlock(bool $hasForeignKeys): void
+    private function unlock(): void
     {
-        // enable "foreign_keys" if they were on...
-        if ($hasForeignKeys) {
-            $this->enableForeignKeys();
+        if ('sqlite' === $this->dbh->getAttribute(PDO::ATTR_DRIVER_NAME)) {
+            $this->dbh->exec('PRAGMA foreign_keys = ON');
         }
+
         // release "lock"
         $this->dbh->exec('DROP TABLE _migration_in_progress');
     }
 
     private function createVersionTable(string $schemaVersion): void
     {
-        // XXX use prepare to specify schema version, even though we have a
-        // strict regexp check
         $this->dbh->exec('CREATE TABLE version (current_version TEXT NOT NULL)');
+        // we know that schemaVersion is a 10 digit string as per
+        // validateSchemaVersion
         $this->dbh->exec(sprintf("INSERT INTO version (current_version) VALUES('%s')", $schemaVersion));
     }
 
