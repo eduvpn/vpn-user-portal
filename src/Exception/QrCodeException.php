aefdc90c664a2bc64ecc990dcf77a710066d86e0
François Kooman <fkooman@tuxed.net>
2021-12-20 20:51:03 +0100

    Revert "remove QrCodeException"
    This reverts commit d25599fe6f3761ae334a7639502618bd2cbc74ab.

---
+++ src/Exception/QrCodeException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+namespace Vpn\Portal\Exception;
+
+use Exception;
+
+class QrCodeException extends Exception
+{
+}
