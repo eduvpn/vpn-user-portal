933578a01eb17293e89c7fca336ec4c44699667b
François Kooman <fkooman@tuxed.net>
2021-12-27 13:27:36 +0100

    update for fkooman/oauth2-server

--- src/Storage.php
+++ src/Storage.php
@@ -20,7 +20,7 @@ class Storage
     public const EXCLUDE_EXPIRED = 2;
     public const EXCLUDE_DISABLED_USER = 4;
 
-    public const CURRENT_SCHEMA_VERSION = '2021122101';
+    public const CURRENT_SCHEMA_VERSION = '2021122701';
 
     private PDO $db;
 
