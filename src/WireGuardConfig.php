49951a497704944dab6396ed6ad59a98daaa1182
François Kooman <fkooman@tuxed.net>
2021-12-28 12:07:41 +0100

    remove apiIpLeaseTime config option for now

---
+++ src/WireGuardConfig.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+namespace Vpn\Portal;
+
+class WireGuardConfig
+{
+    use ConfigTrait;
+
+    private array $configData;
+
+    public function __construct(array $configData)
+    {
+        $this->configData = $configData;
+    }
+
+    public function listenPort(): int
+    {
+        return $this->requireInt('listenPort', 51820);
+    }
+}
