3d6277df46e4b3c6555ddbffaaf90c247bade0e5
François Kooman <fkooman@tuxed.net>
2022-01-03 13:00:11 +0100

    remove (C) header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,13 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * eduVPN - End-user friendly VPN.
- *
- * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
- * SPDX-License-Identifier: AGPL-3.0+
- */
-
 $config = new PhpCsFixer\Config();
 
 return $config->setRules(
