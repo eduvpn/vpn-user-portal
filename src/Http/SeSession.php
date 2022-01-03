8d61d6a313f7fb47259316d14bf576d9f656401c
François Kooman <fkooman@tuxed.net>
2022-01-03 12:57:59 +0100

    update (C) year

--- src/Http/SeSession.php
+++ src/Http/SeSession.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * eduVPN - End-user friendly VPN.
  *
- * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
+ * Copyright: 2016-2022, The Commons Conservancy eduVPN Programme
  * SPDX-License-Identifier: AGPL-3.0+
  */
 
