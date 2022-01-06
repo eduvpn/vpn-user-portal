b5312604563be2a3fda7116511dafba05474ec42
François Kooman <fkooman@tuxed.net>
2022-01-06 21:47:49 +0100

    forgot to add tests/TestVpnApiThreeModule.php

---
+++ tests/TestVpnApiThreeModule.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2022, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+namespace Vpn\Portal\Tests;
+
+use DateTimeImmutable;
+use Vpn\Portal\Config;
+use Vpn\Portal\ConnectionManager;
+use Vpn\Portal\Http\VpnApiThreeModule;
+use Vpn\Portal\ServerInfo;
+use Vpn\Portal\Storage;
+
+class TestVpnApiThreeModule extends VpnApiThreeModule
+{
+    public function __construct(Config $config, Storage $storage, ServerInfo $serverInfo, ConnectionManager $connectionManager)
+    {
+        parent::__construct($config, $storage, $serverInfo, $connectionManager);
+        $this->dateTime = new DateTimeImmutable('2022-01-01T09:00:00+00:00');
+    }
+}
