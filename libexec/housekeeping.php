48a569ce413016d93ff1eca625840cf5af1be702
François Kooman <fkooman@tuxed.net>
2022-01-14 13:53:01 +0100

    aggregate after 1 week, not 1 month

--- libexec/housekeeping.php
+++ libexec/housekeeping.php
@@ -20,23 +20,24 @@ try {
     $config = Config::fromFile($baseDir.'/config/config.php');
     $storage = new Storage($config->dbConfig($baseDir));
 
+    $oneWeekAgo = Dt::get('today -1 week', new DateTimeZone('UTC'));
     $oneMonthAgo = Dt::get('today -1 month', new DateTimeZone('UTC'));
-    $threeDays = Dt::get('now -3 days', new DateTimeZone('UTC'));
+    $threeDaysAgo = Dt::get('now -3 days', new DateTimeZone('UTC'));
 
     // aggregate old entries from the connection statistics
-    $storage->statsAggregate($oneMonthAgo);
+    $storage->statsAggregate($oneWeekAgo);
 
     // remove old entries from the connection log
     $storage->cleanConnectionLog($oneMonthAgo);
 
     // remove old entries from the connection statistics
-    $storage->cleanConnectionStats($oneMonthAgo);
+    $storage->cleanConnectionStats($oneWeekAgo);
 
     // delete expired WireGuard peers and OpenVPN certificates
-    $storage->cleanExpiredConfigurations($threeDays);
+    $storage->cleanExpiredConfigurations($threeDaysAgo);
 
     // delete expires OAuth authorizations
-    $storage->cleanExpiredOAuthAuthorizations($threeDays);
+    $storage->cleanExpiredOAuthAuthorizations($threeDaysAgo);
 } catch (Exception $e) {
     echo 'ERROR: '.$e->getMessage().\PHP_EOL;
 
