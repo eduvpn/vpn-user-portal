9edd42656ba56b707fb292b1187ec4c75428ee32
Maurice Makaay <account+github@makaay.nl>
2022-06-09 16:04:01 +0000

    Add tests for proxied requests

--- tests/Http/RequestTest.php
+++ tests/Http/RequestTest.php
@@ -37,4 +37,44 @@ final class RequestTest extends TestCase
         static::expectExceptionMessage('invalid value for "xyz"');
         $r->requirePostParameter('xyz', function ($s): void { throw new RangeException(); });
     }
+
+    public function testNotProxiedRequest(): void
+    {
+        # Purposely testing using HTTP on port 80 here, to make it possible to
+        # build on top of this proof in the proxied request test, where HTTPS on
+        # port 443 must override these properties.
+        $r = new Request(
+            [
+                'SERVER_NAME' => 'the.server.name',
+                'SERVER_PORT' => '80',
+                'HTTPS' => 'off'
+            ],
+            [],
+            [],
+            []
+        );
+        static::assertSame('http', $r->getScheme());
+        static::assertSame('the.server.name', $r->getServerName());
+        static::assertSame(80, $r->getServerPort());
+    }
+
+    public function testProxiedRequest(): void
+    {
+        $r = new Request(
+            [
+                'SERVER_NAME' => 'internal.server.name',
+                'SERVER_PORT' => '80',
+                'HTTPS' => 'off',
+                'HTTP_X_FORWARDED_PROTO' => 'https',
+                'HTTP_X_FORWARDED_HOST' => 'external.server.name',
+                'HTTP_X_FORWARDED_PORT' => '443',
+            ],
+            [],
+            [],
+            []
+        );
+        static::assertSame('https', $r->getScheme());
+        static::assertSame('external.server.name', $r->getServerName());
+        static::assertSame(443, $r->getServerPort());
+    }
 }
