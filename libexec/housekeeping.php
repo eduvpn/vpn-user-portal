11c2711a595044f4fa0af62575225c12f89ba903
François Kooman <fkooman@tuxed.net>
2022-12-16 09:04:12 +0100

    log a warning when generating aggregate stats takes >5 seconds

--- libexec/housekeeping.php
+++ libexec/housekeeping.php
@@ -15,6 +15,9 @@ $baseDir = dirname(__DIR__);
 use Vpn\Portal\Cfg\Config;
 use Vpn\Portal\Dt;
 use Vpn\Portal\Storage;
+use Vpn\Portal\SysLogger;
+
+$logger = new SysLogger('vpn-user-portal');
 
 try {
     $config = Config::fromFile($baseDir.'/config/config.php');
@@ -25,7 +28,12 @@ try {
     $startOfTheDay = Dt::get('today', new DateTimeZone('UTC'));
 
     // aggregate old entries from the connection statistics
+    $startTime = time();
     $storage->statsAggregate($oneWeekAgo);
+    $elapsedTime = time() - $startTime;
+    if ($elapsedTime > 5) {
+        $logger->warning(sprintf('generating aggregate statistics took %ds', $elapsedTime));
+    }
 
     // remove old entries from the connection statistics
     $storage->cleanLiveStats($oneWeekAgo);
