7026a8d8ec61489f7ef2f95e850f0f634e140249
François Kooman <fkooman@tuxed.net>
2023-01-16 10:36:01 +0100

    sort the OAuth clients by branding

--- src/OAuth/ClientDb.php
+++ src/OAuth/ClientDb.php
@@ -18,21 +18,15 @@ class ClientDb extends SimpleClientDb
 {
     public function __construct()
     {
+        //
+        // eduVPN
+        //
         $this->add(
             new ClientInfo(
                 'org.eduvpn.app.windows',
                 ['http://127.0.0.1:{PORT}/callback', 'http://[::1]:{PORT}/callback'],
                 null,
                 'eduVPN for Windows',
-                true
-            )
-        );
-        $this->add(
-            new ClientInfo(
-                'org.letsconnect-vpn.app.windows',
-                ['http://127.0.0.1:{PORT}/callback', 'http://[::1]:{PORT}/callback'],
-                null,
-                'Let\'s Connect! for Windows',
                 true
             )
         );
@@ -47,28 +41,10 @@ class ClientDb extends SimpleClientDb
         );
         $this->add(
             new ClientInfo(
-                'org.letsconnect-vpn.app.android',
-                ['org.letsconnect-vpn.app:/api/callback'],
-                null,
-                'Let\'s Connect! for Android',
-                true
-            )
-        );
-        $this->add(
-            new ClientInfo(
                 'org.eduvpn.app.ios',
                 ['org.eduvpn.app.ios:/api/callback'],
                 null,
                 'eduVPN for iOS',
-                true
-            )
-        );
-        $this->add(
-            new ClientInfo(
-                'org.letsconnect-vpn.app.ios',
-                ['org.letsconnect-vpn.app.ios:/api/callback'],
-                null,
-                'Let\'s Connect! for iOS',
                 true
             )
         );
@@ -83,19 +59,50 @@ class ClientDb extends SimpleClientDb
         );
         $this->add(
             new ClientInfo(
-                'org.letsconnect-vpn.app.macos',
+                'org.eduvpn.app.linux',
                 ['http://127.0.0.1:{PORT}/callback', 'http://[::1]:{PORT}/callback'],
                 null,
-                'Let\'s Connect! for macOS',
+                'eduVPN for Linux',
+                true
+            )
+        );
+
+        //
+        // Let's Connect!
+        //
+        $this->add(
+            new ClientInfo(
+                'org.letsconnect-vpn.app.windows',
+                ['http://127.0.0.1:{PORT}/callback', 'http://[::1]:{PORT}/callback'],
+                null,
+                'Let\'s Connect! for Windows',
                 true
             )
         );
         $this->add(
             new ClientInfo(
-                'org.eduvpn.app.linux',
+                'org.letsconnect-vpn.app.android',
+                ['org.letsconnect-vpn.app:/api/callback'],
+                null,
+                'Let\'s Connect! for Android',
+                true
+            )
+        );
+        $this->add(
+            new ClientInfo(
+                'org.letsconnect-vpn.app.ios',
+                ['org.letsconnect-vpn.app.ios:/api/callback'],
+                null,
+                'Let\'s Connect! for iOS',
+                true
+            )
+        );
+        $this->add(
+            new ClientInfo(
+                'org.letsconnect-vpn.app.macos',
                 ['http://127.0.0.1:{PORT}/callback', 'http://[::1]:{PORT}/callback'],
                 null,
-                'eduVPN for Linux',
+                'Let\'s Connect! for macOS',
                 true
             )
         );
