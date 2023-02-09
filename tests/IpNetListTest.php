8889231e8e0f7e41be825052620d48efc3681ee0
François Kooman <fkooman@tuxed.net>
2023-02-09 22:55:35 +0100

    add some more IpNetList examples

--- tests/IpNetListTest.php
+++ tests/IpNetListTest.php
@@ -46,6 +46,30 @@ final class IpNetListTest extends TestCase
         );
     }
 
+    public function testNoPrivateIpFour(): void
+    {
+        $ipList = new IpNetList([Ip::fromIpPrefix('0.0.0.0/0')]);
+        $ipList->remove(Ip::fromIpPrefix('10.0.0.0/8'));
+        $ipList->remove(Ip::fromIpPrefix('172.16.0.0/12'));
+        $ipList->remove(Ip::fromIpPrefix('192.168.0.0/16'));
+
+        static::assertSame(
+            '[0.0.0.0/5 8.0.0.0/7 11.0.0.0/8 12.0.0.0/6 16.0.0.0/4 32.0.0.0/3 64.0.0.0/2 128.0.0.0/3 160.0.0.0/5 168.0.0.0/6 172.0.0.0/12 172.32.0.0/11 172.64.0.0/10 172.128.0.0/9 173.0.0.0/8 174.0.0.0/7 176.0.0.0/4 192.0.0.0/9 192.128.0.0/11 192.160.0.0/13 192.169.0.0/16 192.170.0.0/15 192.172.0.0/14 192.176.0.0/12 192.192.0.0/10 193.0.0.0/8 194.0.0.0/7 196.0.0.0/6 200.0.0.0/5 208.0.0.0/4 224.0.0.0/3]',
+            (string) $ipList
+        );
+    }
+
+    public function testNoPrivateIpSix(): void
+    {
+        $ipList = new IpNetList([Ip::fromIpPrefix('::/0')]);
+        $ipList->remove(Ip::fromIpPrefix('fc00::/7'));
+
+        static::assertSame(
+            '[::/1 8000::/2 c000::/3 e000::/4 f000::/5 f800::/6 fe00::/7]',
+            (string) $ipList
+        );
+    }
+
     public function testAdd(): void
     {
         $ipList = new IpNetList();
