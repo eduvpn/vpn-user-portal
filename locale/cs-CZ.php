cea87e8d2305b5d0d26359507498610c594d55ba
Ivo Durnik <durnik@ics.muni.cz>
2023-09-07 10:55:44 +0200

    fix: Not needed to change code comment

--- locale/cs-CZ.php
+++ locale/cs-CZ.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * eduVPN - Uživatelsky přívětivá VPN.
+ * eduVPN - End-user friendly VPN.
  *
  * Copyright: 2014-2023, The Commons Conservancy eduVPN Programme
  * SPDX-License-Identifier: AGPL-3.0+
